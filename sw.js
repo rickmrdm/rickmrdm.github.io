26a8a18d849692fff77127c9eabacf4f7737ad3f
form43 <form43github@gmail.com>
2021-04-28 18:28:32 -0500

    Cambio

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "gilpgdm-1.15";
+const CACHE = "rickmrdm-1.16";
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
