c92958f280ebd69e08bae75ba6584829fd1bb250
rickmrdm <rickmrdm@gmail.com>
2021-07-14 20:37:47 -0500

    Cambio

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const CACHE = "rickmrdm-1.25";
+const CACHE = "rickmrdm-1.26";
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
